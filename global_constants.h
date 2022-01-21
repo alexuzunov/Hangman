db177edaac660357e827be7294fc517a0ae7843b
alexuzunov <alex.uzunov2002@gmail.com>
2022-01-21 20:32:19 +0200

    Menus and Dictionary
    Configured rendering of both the main menu and the settings menu and added a dictionary consisting of 465 words.

---
+++ global_constants.h
@@ -0,0 +1,9 @@
+#ifndef GLOBAL_CONSTANTS_H
+#define GLOBAL_CONSTANTS_H
+
+const std::string mainMenuOptions[3] = {"Play", "Settings", "Quit"};
+const std::string settingsOptions[2] = {"Set Limit (Lower and Upper)", "Set Attempt Count"};
+const int defaultLowerLimit = 3, defaultUpperLimit = 12;
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "render.h"
+
+int main() {
+    renderMainMenu();
+
+    return 0;
+}
